8d358c82c3e283ca8a56923981580857f5934e2f
lemitaye <tayelemi@gmail.com>
2023-01-03 06:15:36 +0300

    resolved variable label names and national est.

--- 1_do_files/tabs.R
+++ 1_do_files/tabs.R
@@ -2,6 +2,8 @@
 library(haven)
 library(tidyverse)
 library(thatssorandom)
+library(labelled)
+
 
 setwd("C:/Users/tayel/Dropbox/Documents/SPIA/Ethiopia")
 
@@ -15,8 +17,9 @@ select_hh_level <- function(tbl, pw) {
   tbl %>% 
     mutate_if(is.labelled, as.character, levels = "labels") %>% 
     select(
-      household_id, region, {{pw}}, wave, hhd_ofsp, hhd_awassa83, hhd_rdisp, hhd_motorpump, hhd_swc, hhd_consag1, hhd_consag2, 
-      hhd_affor, hhd_mango, hhd_papaya, hhd_avocado, hhd_impcr13, hhd_impcr19, hhd_impcr11, 
+      household_id, region, {{pw}}, wave, hhd_ofsp, hhd_awassa83, hhd_rdisp, 
+      hhd_motorpump, hhd_swc, hhd_consag1, hhd_consag2, hhd_affor, hhd_mango, 
+      hhd_papaya, hhd_avocado, hhd_impcr13, hhd_impcr19, hhd_impcr11, 
       hhd_impcr24, hhd_impcr14, hhd_impcr3, hhd_impcr5, hhd_impcr60, hhd_impcr62
     ) %>% 
     mutate(region = recode(region, 
@@ -41,36 +44,80 @@ hh_level_w4 <- select_hh_level(wave4_hh_new, pw_w4) %>%
 
 
 
-mean_tbl <- function(tbl) {
+mean_tbl <- function(tbl, by_region = TRUE) {
   
-  tbl %>% 
-    group_by(wave, region) %>% 
-    summarise(
-      across(hhd_ofsp:hhd_impcr62, 
-             ~ weighted.mean(., w = pw, na.rm = TRUE)),
-      .groups = "drop"
-    ) %>% 
-    pivot_longer(hhd_ofsp:hhd_impcr62, 
-                 names_to = "variable",
-                 values_to = "mean")
+  if (by_region) {
+    
+    tbl %>% 
+      group_by(wave, region) %>% 
+      summarise(
+        across(hhd_ofsp:hhd_impcr62, 
+               ~ weighted.mean(., w = pw, na.rm = TRUE)),
+        .groups = "drop"
+      ) %>% 
+      pivot_longer(hhd_ofsp:hhd_impcr62, 
+                   names_to = "variable",
+                   values_to = "mean")
+    
+  } else {
+    
+    tbl %>% 
+      summarise(
+        across(hhd_ofsp:hhd_impcr62, 
+               ~ weighted.mean(., w = pw, na.rm = TRUE))
+      ) %>% 
+      pivot_longer(hhd_ofsp:hhd_impcr62, 
+                   names_to = "variable",
+                   values_to = "mean")
+    
+  }
   
 }
 
-hh_level_mean <- bind_rows(
+
+labels <- var_label(hh_level_w5) %>% 
+  .[-c(1:4)] %>% 
+  as_tibble() %>% 
+  pivot_longer(
+    cols = everything(), 
+    names_to = "variable", 
+    values_to = "label"
+    ) 
+  
+
+regions_hh_level <- bind_rows(
   mean_tbl(hh_level_w4),
   mean_tbl(hh_level_w5)
   ) %>% 
   mutate(wave = paste("wave", wave) %>% 
-           fct_relevel("wave 5", "wave 4"))
+           fct_relevel("wave 5", "wave 4")) %>% 
+  left_join(labels, by = "variable")
+
+national_hh_level <- bind_rows(
+  mean_tbl(hh_level_w4, by_region = FALSE) %>% 
+    mutate(wave = "wave 4"),
+  mean_tbl(hh_level_w5, by_region = FALSE) %>% 
+    mutate(wave = "wave 5")
+) %>% 
+  mutate(wave = fct_relevel(wave, "wave 5", "wave 4")) %>% 
+  left_join(labels, by = "variable")
 
 
-hh_level_mean %>% 
+regions_hh_level %>% 
   filter(region != "Tigray") %>% 
-  ggplot(aes(mean, variable, fill = wave)) +
+  ggplot(aes(mean, label, fill = wave)) +
   geom_col(position = "dodge") +
-  facet_wrap(~ region)
+  facet_wrap(~ region, scales = "free_y")
+
+national_hh_level %>% 
+  ggplot(aes(mean, label, fill = wave)) +
+  geom_col(position = "dodge")
 
 
+
+# Add no. of obs
+
+# Next: EA level (replicate first in stata)
 
 
 
