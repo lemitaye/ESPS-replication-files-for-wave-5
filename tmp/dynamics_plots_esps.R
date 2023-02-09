14c40145f4362ad3e2d7c7eb08bfe034f112d7bd
lemitaye <tayelemi@gmail.com>
2023-02-09 23:46:45 +0100

    added maize DNA variables

--- tmp/dynamics_plots_esps.R
+++ tmp/dynamics_plots_esps.R
@@ -15,18 +15,20 @@ animal_agri <- bind_rows(
 ) %>% 
   select(-variable, -improv) %>% 
   mutate(
-    label = recode(label,
-                   "AI on any livestock type - both public & private" = "Artificial insemination use", 
-                   "Livestock AI - both public & private" = "Artificial insemination use", 
-                   "Large ruminants crossbred" = "Crossbred LARGE RUMINANTS",
-                   "Small ruminants crossbred" = "Crossbred SMALL RUMINANTS",
-                   "Poultry crossbred" = "Crossbred POULTRY"
+    label = recode(
+      label,
+      "AI on any livestock type - both public & private" = "Artificial insemination use", 
+      "Livestock AI - both public & private" = "Artificial insemination use", 
+      "Large ruminants crossbred" = "Crossbred LARGE RUMINANTS",
+      "Small ruminants crossbred" = "Crossbred SMALL RUMINANTS",
+      "Poultry crossbred" = "Crossbred POULTRY",
+      "Feed and forages: Elephant grass, Sesbaniya, & Alfalfa" = "Forages"             
     )) %>%  
   filter(label %in% c(
     "Crossbred LARGE RUMINANTS",
-    "Crossbred SMALL RUMINANTS",
     "Crossbred POULTRY",
-    "Artificial insemination use"))
+    "Artificial insemination use",
+    "Forages"))
 
 animal_dyn_plt <- animal_agri %>% 
   mutate(label = str_to_sentence(label)) %>% 
@@ -176,10 +178,19 @@ swc_affor <- bind_rows(
   mutate(national_hh_panel, level = "Household-level (only panel)"),
   mutate(national_ea_level, level = "EA-level")
 ) %>% 
-  select(-variable, -improv) %>% 
+  select(-variable, -improv) %>%
+  mutate(
+    label = recode(
+      label,
+      "River dispersion" = "River diversion",
+      "Motor pump used for irrigation" = "Motorized pumps",
+      "Motor pump" = "Motorized pumps"
+    )) %>% 
   filter(label %in% c(
     "Soil Water Conservation practices",
-    "Afforestation"))
+    "Afforestation",
+    "River diversion",
+    "Motorized pumps"))
 
 
 swc_aff_dyn_plt <- swc_affor %>% 
@@ -194,7 +205,7 @@ swc_aff_dyn_plt <- swc_affor %>%
   # expand_limits(y = .6) +
   facet_wrap(~level, nrow = 1, scales = "free_y") +
   labs(x = "", y = "Percent",
-       title = "Natural resource management practices: Afforestation and SWC",
+       title = "Natural resource management practices",
        fill = "",
        caption = "Percent at the household level are weighted sample means (using wave 5 weights)") +
   scale_fill_Publication() + 
@@ -267,11 +278,99 @@ ggsave(
 ) 
 
 
+# Comparing joint adoption rates:
 
 
+nrm_joint <- joint_rate_tbl %>% 
+  filter(str_detect(label, "NRM")) %>% 
+  filter(variable %in% c("nrm_ca", "nrm_tree", "nrm_animal", "nrm_breed",
+                         "nrm_psnp")) 
+
+ca_joint <- joint_rate_tbl %>% 
+  filter(str_detect(label, "CA")) %>% 
+  filter(variable %in% c("ca_tree", "ca_animal", "ca_breed", "ca_psnp")) 
+
+agrfrst_joint <- joint_rate_tbl %>% 
+  filter(str_detect(label, "Trees|Breeding")) %>% 
+  filter(variable %in% c("tree_animal", "tree_breed", "tree_psnp", "breed_psnp")) 
 
 
+maize_joint <- joint_rate_tbl %>% 
+  filter(str_detect(label, "Maize")) %>% 
+  filter(variable %in% c("nrm_maize", "ca_maize", "tree_maize", "animal_maize",
+                         "breed_maize", "psnp_maize"))
 
+
+plot_joint <- function(tbl) {
+  
+  tbl %>% 
+    ggplot(aes(label, joint_rate, fill = wave)) +
+    geom_col(position = "dodge") +
+    geom_text(aes(label = paste0( round(joint_rate*100, 1) ) ),
+              position = position_dodge(width = 1),
+              vjust = -.35, size = 3) +
+    scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
+    scale_y_continuous(labels = percent_format()) +
+    labs(x = "", y = "Percent",
+         title = "Comparing joint adoption rates across waves",
+         subtitle = "Data at the household level",
+         fill = "") +
+    scale_fill_Publication() + 
+    theme_Publication() +
+    theme(
+      legend.position = "top",
+      legend.margin = margin(t = -0.4, unit = "cm"),
+      # plot.caption = element_text(hjust = 0),
+      axis.title = element_text(size = 12.5),
+      plot.margin = unit(c(1, 1, 0.5, 1), units = "line") # top, right, bottom, & left
+    )
+  
+}
+
+
+nrm_joint_plt <- plot_joint(nrm_joint) +
+  labs(
+    caption = "NRM = AWM + SWC practices; CA = Conservation agriculture. 
+    Percent are weighted sample means using each wave's respective weights."
+  )
+
+ca_joint_plt <- plot_joint(ca_joint) +
+  labs(
+    caption = "CA = Conservation agriculture.
+    Percent are weighted sample means using each wave's respective weights"
+    )
+
+agrfrst_joint_plt <- plot_joint(agrfrst_joint) +
+  labs(
+    caption = "Percent are weighted sample means using each wave's respective weights"
+  )
+
+plot_joint(maize_joint)
+
+
+joint_plots <- list(nrm_joint_plt, ca_joint_plt, agrfrst_joint_plt)
+
+names(joint_plots) <- c("nrm_joint_plt", "ca_joint_plt", "agrfrst_joint_plt")
+
+for (i in seq_along(joint_plots)) {
+  
+  file <- paste0("LSMS_W5/tmp/figures/", names(joint_plots)[[i]], ".pdf")
+  
+  print(paste("saving to", file))
+  
+  ggsave(
+    filename = file,
+    plot = joint_plots[[i]],
+    device = cairo_pdf,
+    width = 6.58,
+    height = 5.31#,
+    # scale = 1.2#,
+    # units = "mm"
+  )
+}
+
+# relabel x-axis labels
+# add maize-dna
 
 
 
