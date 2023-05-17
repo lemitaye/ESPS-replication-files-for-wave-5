95cb2dfede111f3a8fe10630e0757bb6cfd76648
lemitaye <53483727+lemitaye@users.noreply.github.com>
2023-05-17 17:10:58 +0200

    comparing CA among panel DNA hhs

--- 5_analysis/03_new_innov_ess5.R
+++ 5_analysis/03_new_innov_ess5.R
@@ -1242,3 +1242,76 @@ ggsave(
   height = 200,
   units = "mm"
 ) 
+
+
+# CA only among hhs with DNA data ----------
+
+
+hh_panel_dna_w4 <- hh_panel_w4 %>% 
+  semi_join(ess4_dna_hh_panel, by = "household_id")
+
+hh_panel_dna_w5 <- hh_panel_w5 %>% 
+  semi_join(ess5_dna_hh_panel, by = "household_id")
+
+national_hh_panel_dna <- bind_rows(
+  mean_tbl(hh_panel_dna_w4, group_vars = c("wave", "variable"), pw = pw_panel),
+  mean_tbl(hh_panel_dna_w5, group_vars = c("wave", "variable"), pw = pw_panel)
+) %>% 
+  mutate(wave = paste("Wave", wave)) %>% 
+  left_join(labels, by = "variable") %>% 
+  select(wave, variable, label, mean, nobs)  
+
+regions_hh_panel_dna <- bind_rows(
+  mean_tbl(hh_panel_dna_w4, group_vars = c("wave", "variable", "region"), pw = pw_panel),
+  mean_tbl(hh_panel_dna_w5, group_vars = c("wave", "variable", "region"), pw = pw_panel)
+) %>% 
+  mutate(wave = paste("Wave", wave)) %>% 
+  left_join(labels, by = "variable") %>% 
+  select(wave, region, variable, label, mean, nobs)
+
+
+adopt_rates_panel_hh_dna <- bind_rows(
+  regions_hh_panel_dna, 
+  national_hh_panel_dna %>% 
+    mutate(region = "National")
+) %>% 
+  mutate(region = fct_relevel(
+    region, 
+    "Amhara", "Oromia", "SNNP", "Harar", "Dire Dawa", "National"
+  ))
+
+
+adopt_rates_panel_hh_dna %>% 
+  filter(variable %in% c("hhd_consag1"), region != "Tigray") %>% 
+  plot_waves() +
+  expand_limits(y = .7) +
+  labs(title = "Conservation Agriculture with Minimum Tillage",
+       subtitle = "Panel DNA sample only")
+
+adopt_rates_panel_hh_dna %>% 
+  filter(variable %in% c("hhd_consag2"), region != "Tigray") %>% 
+  plot_waves() +
+  expand_limits(y = .025) +
+  labs(title = "Conservation Agriculture with Zero Tillage",
+       subtitle = "Panel DNA sample only")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
