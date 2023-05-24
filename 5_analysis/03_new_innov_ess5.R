26b9f1bbfc0be835e4a0546baccc62de0db339df
lemitaye <53483727+lemitaye@users.noreply.github.com>
2023-05-24 11:54:10 +0200

    computed weighted means

--- 5_analysis/03_new_innov_ess5.R
+++ 5_analysis/03_new_innov_ess5.R
@@ -1315,7 +1315,7 @@ prod_assets <- map(
       mutate(HHown_item = case_when(
         s11q00 == 2 ~ 0, is.na(s11q00) ~ 0, .default = as.numeric(s11q00))
       ) %>% 
-      select(wave, household_id, asset_cd, HHown_item) %>% 
+      select(wave, household_id, asset_cd, HHown_item, starts_with("pw")) %>% 
       filter(asset_cd >= 29) %>% 
       mutate_if(is.labelled, as_factor)
       
@@ -1326,27 +1326,66 @@ prod_assets <- map(
 prod_assets_rural <- prod_assets %>% 
   # retain only rural households (vs. all hhs, i.e., urban included)
   inner_join( 
-    select(track_hh, household_id, hh_status, starts_with("pw")),
+    select(track_hh, household_id, hh_status, pw_panel),
     by = "household_id"
-    ) 
+    ) %>% 
+  mutate(
+    pw_wv = case_when(is.na(pw_w5) ~ pw_w4, is.na(pw_w4) ~ pw_w5),
+    asset_cd = str_remove(asset_cd, "(^\\d+\\s+)")
+    ) %>% 
+  select(-pw_w4, -pw_w5) 
 
-prod_assets_rural %>% 
+pset_tab_all <- prod_assets_rural %>% 
   group_by(wave, asset_cd) %>% 
   summarize(
-    mean = mean(HHown_item, na.rm = T),
+    mean = weighted.mean(HHown_item, w = pw_wv, na.rm = T),
     n = sum(!is.na(HHown_item)),
     .groups = "drop"
     ) %>% 
-  pivot_wider(names_from = wave, values_from = c(mean, n))
+  pivot_wider(names_from = wave, values_from = c(mean, n)) %>% 
+  clean_names() %>% 
+  select(asset_cd, mean_wave_4, n_wave_4, mean_wave_5, n_wave_5) %>% 
+  mutate(across( starts_with("mean_wave"), ~round(. * 100, 2) ))
 
-prod_assets_rural %>% 
+pset_tab_panel <- prod_assets_rural %>% 
   filter(hh_status==3) %>% 
   group_by(wave, asset_cd) %>% 
   summarize(
-    mean = mean(HHown_item, na.rm = T),
+    mean = weighted.mean(HHown_item, w = pw_panel, na.rm = T),
     n = sum(!is.na(HHown_item)),
+    .groups = "drop"
   ) %>% 
-  pivot_wider(names_from = wave, values_from = c(mean, n))
+  pivot_wider(names_from = wave, values_from = c(mean, n)) %>% 
+  clean_names() %>% 
+  select(asset_cd, mean_wave_4, n_wave_4, mean_wave_5, n_wave_5) %>% 
+  mutate(across( starts_with("mean_wave"), ~round(. * 100, 2) ))
+
+kbl(
+  pset_tab_all,
+  caption = "Percentage of households with productive assets - all houesholds in both waves",
+  booktabs = TRUE,
+  linesep = "",
+  align = c("l", "c", "c", "c"),
+  col.names = c("Productive asset", "Mean (%)", "N", "Mean (%)", "N")
+) %>% 
+  add_header_above(c(" ", "Wave 4" = 2, "Wave 5" = 2)) %>% 
+  # column_spec(2:7, width = "5em", latex_valign = "b") %>% 
+  kable_styling(latex_options = c("striped", "hold_position")) %>% 
+  save_kable("tables/15_SR_names_unidentified_clusters.tex")
+
+
+kbl(
+  pset_tab_panel,
+  caption = "Percentage of households with productive assets - panel houesholds",
+  booktabs = TRUE,
+  linesep = "",
+  align = c("l", "c", "c", "c"),
+  col.names = c("Productive asset", "Mean (%)", "N", "Mean (%)", "N")
+) %>% 
+  add_header_above(c(" ", "Wave 4" = 2, "Wave 5" = 2)) %>% 
+  # column_spec(2:7, width = "5em", latex_valign = "b") %>% 
+  kable_styling(latex_options = c("striped", "hold_position")) %>% 
+  save_kable("tables/15_SR_names_unidentified_clusters.tex")
 
 
 
