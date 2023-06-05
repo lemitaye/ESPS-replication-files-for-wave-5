c30f132b476788f8768b5b221cd6c38b526f492a
lemitaye <53483727+lemitaye@users.noreply.github.com>
2023-06-05 06:04:19 +0200

    added proportion of crop growing households

--- 5_analysis/07_innov_ess4.R
+++ 5_analysis/07_innov_ess4.R
@@ -20,8 +20,6 @@ root <- "C:/Users/l.daba/SPIA Dropbox/SPIA General/5. OBJ.3 - Data collection/Co
 
 ess4_hh_all <- read_dta(file.path(root, "tmp/dynamics/ess4_hh_all.dta"))
 
-ess4_hh_psnp <- read_dta(file.path(root, w4_dir, "ess4_hh_psnp.dta"))
-
 # psnp data (generated in 03_new_innov_ess5.R)
 psnp_hh <- read_csv("dynamics_presentation/data/psnp_hh.csv")
 
@@ -55,8 +53,7 @@ innovs <- c(
   "hhd_livIA", "hhd_cross_largerum", "hhd_cross_smallrum", "hhd_cross_poultry", 
   "hhd_gaya", "hhd_sasbaniya", "hhd_alfa", "hhd_elepgrass",
   "hhd_grass", "hhd_ofsp", "hhd_awassa83", "hhd_rdisp", "hhd_motorpump", "hhd_swc", 
-  "hhd_consag1", "hhd_consag2", "hhd_affor", "hhd_mango", "hhd_papaya", "hhd_avocado",
-  "qpm", "dtmz", "maize_cg", "barley_cg", "sorghum_cg"
+  "hhd_consag1", "hhd_consag2", "hhd_affor", "hhd_mango", "hhd_papaya", "hhd_avocado"
 )
 
 
@@ -132,10 +129,68 @@ write_csv(adopt_rates_w4_hh, file = "dynamics_presentation/data/adopt_rates_w4_h
 
 
 
+# DNA -------
+
+dna_vars <- c("qpm", "dtmz", "maize_cg", "barley_cg", "sorghum_cg",
+              "cr1", "cr2", "cr6")
+
+hh_dna_w4 <- ess4_hh_all %>% 
+  select(
+    household_id, ea_id, wave, region = saq01, starts_with("pw_"), all_of(dna_vars),
+    cr1, cr2, cr6
+  ) %>% 
+  mutate(
+    across(
+      c(all_of(dna)), ~recode(., `100` = 1))  # since w4 vars are mult. by 100
+  ) %>% 
+  recode_region() %>% 
+  filter(region %in% c("Amhara", "Dire Dawa", "Harar", "Oromia", "SNNP", "Tigray"))
+
+
+labs_dna <- ess4_hh_all %>% 
+  select(all_of(dna_vars)) %>% 
+  var_label() %>% 
+  as_tibble() %>% 
+  pivot_longer(
+    cols = everything(), 
+    names_to = "variable", 
+    values_to = "label"
+  )
+
+
+dna_w4 <- bind_rows(
+  mean_tbl(hh_dna_w4, dna_vars, group_vars = c("wave", "variable"), pw = pw_w4) %>% 
+    mutate(region = "National"),
+  mean_tbl(hh_dna_w4, dna_vars, group_vars = c("wave", "variable", "region"), pw = pw_w4) 
+ ) %>% 
+  filter(!variable %in% c("cr1", "cr2", "cr6")) %>% 
+  left_join(labs_dna, by = "variable") %>% 
+  mutate(wave = paste("Wave", wave)) %>% 
+  select(wave, region, variable, label, mean, nobs) %>% 
+  mutate(crop = case_match(
+    variable,
+    c("maize_cg", "dtmz", "qpm") ~ "Maize",
+    "sorghum_cg" ~ "Sorghum", 
+    "barley_cg" ~ "Barley"
+  ))
+
+
+crop_growing <- adopt_rate_dna_w4 %>% 
+  filter(variable %in% c("cr1", "cr2", "cr6")) %>% 
+  mutate(crop = case_match(
+    variable,
+    "cr2" ~ "Maize",
+    "cr6" ~ "Sorghum", 
+    "cr1" ~ "Barley"
+  )) %>% 
+  select(region, crop, growing_pct = mean) 
+
+adopt_rate_dna_w4 <- dna_w4 %>% 
+  left_join(crop_growing, by = c("region", "crop"))
 
 
 
-
+write_csv(adopt_rate_dna_w4, file = "dynamics_presentation/data/adopt_rate_dna_w4.csv")
 
 
 
