596569eafbdcb8a16d1518440731d301744045cb
lemitaye <53483727+lemitaye@users.noreply.github.com>
2023-06-10 06:24:12 +0200

    four lower bounds

--- 5_analysis/07_innov_ess4.R
+++ 5_analysis/07_innov_ess4.R
@@ -20,17 +20,20 @@ root <- "C:/Users/l.daba/SPIA Dropbox/SPIA General/5. OBJ.3 - Data collection/Co
 
 ess4_hh_all <- read_dta(file.path(root, "tmp/dynamics/ess4_hh_all.dta"))
 
+ess4_bounds <- read_dta(file.path(root, "tmp/dynamics/ess4_bounds.dta"))
+
+
 # psnp data (generated in 03_new_innov_ess5.R)
 psnp_hh <- read_csv("dynamics_presentation/data/psnp_hh.csv")
 
 
-recode_region <- function(tbl) {
+recode_region <- function(tbl, region_var = region) {
   
   suppressWarnings(
     tbl %>% 
       mutate(
         region = recode(
-          region, 
+          {{region_var}}, 
           `1` = "Tigray",
           `2` = "Afar",
           `3` = "Amhara",
@@ -143,8 +146,7 @@ hh_dna_w4 <- ess4_hh_all %>%
     across(
       c(all_of(dna)), ~recode(., `100` = 1))  # since w4 vars are mult. by 100
   ) %>% 
-  recode_region() %>% 
-  filter(region %in% c("Amhara", "Dire Dawa", "Harar", "Oromia", "SNNP", "Tigray"))
+  recode_region() 
 
 
 labs_dna <- ess4_hh_all %>% 
@@ -172,18 +174,26 @@ dna_w4 <- bind_rows(
     c("maize_cg", "dtmz", "qpm") ~ "Maize",
     "sorghum_cg" ~ "Sorghum", 
     "barley_cg" ~ "Barley"
-  ))
+  )) %>% 
+  filter(region %in% c("Amhara", "Dire Dawa", "Harar", "Oromia", "SNNP", "Tigray"))
 
 
-crop_growing <- adopt_rate_dna_w4 %>% 
-  filter(variable %in% c("cr1", "cr2", "cr6")) %>% 
+crop_growing <- hh_dna_w4 %>% 
+  select(c("cr1", "cr2", "cr6"), region, pw_w4) %>% 
+  group_by(region) %>% 
+  summarise(
+    across( c(cr1, cr2, cr6), ~weighted.mean(., w = pw_w4, na.rm = T) )
+    ) %>% 
+  pivot_longer(
+    c(cr1, cr2, cr6), names_to = "variable", values_to = "growing_pct"
+    ) %>% 
   mutate(crop = case_match(
     variable,
     "cr2" ~ "Maize",
     "cr6" ~ "Sorghum", 
     "cr1" ~ "Barley"
   )) %>% 
-  select(region, crop, growing_pct = mean) 
+  select(region, crop, growing_pct) 
 
 adopt_rate_dna_w4 <- dna_w4 %>% 
   left_join(crop_growing, by = c("region", "crop"))
@@ -193,6 +203,58 @@ adopt_rate_dna_w4 <- dna_w4 %>%
 write_csv(adopt_rate_dna_w4, file = "dynamics_presentation/data/adopt_rate_dna_w4.csv")
 
 
+
+
+# Upper and lower bound estimates -----------
+
+## Wave 4 ---------
+
+no_dna_reg <- c("Afar", "Benishangul Gumuz", "Gambela", "Somali")
+
+bounds_w4 <- ess4_bounds %>% 
+  recode_region(saq01) %>% 
+  group_by(region) %>% 
+  summarise(
+    across(c(starts_with(c("ubound", "lbound")), cr2, anycr), ~weighted.mean(., w = pw_w4, na.rm = T))
+  ) %>% 
+  mutate(across(-region, ~replace_na(., 0))) %>% 
+  rename(maize_pct = cr2, anycr_pct = anycr)
+
+
+bd_means_w4 <- bounds_w4 %>% 
+  mutate(
+    maize_pct = if_else(region %in% no_dna_reg, 1, maize_pct),
+    anycr_pct = if_else(region %in% no_dna_reg, 1, anycr_pct)
+    ) %>% 
+  mutate(
+    mean_ub = ( ubound1a * maize_pct ) + ( ubound1b * (1 - maize_pct) ),
+    
+    mean_lb1 = ( lbound1 * maize_pct ),
+    
+    mean_lb2 = ( lbound2a * maize_pct ) + ( lbound2b * (1 - maize_pct) ),
+    
+    mean_lb3 = ( lbound3 * anycr_pct ),
+    
+    mean_lb4 = ( lbound4a * anycr_pct ) + ( lbound4b * (1 - anycr_pct) ),
+  ) %>% 
+  select(region, starts_with("mean_"))
+
+
+bounds_w4 %>% 
+  pivot_longer(-region, names_to = "variable", values_to = "mean") %>% 
+  filter(variable != "ubound3") %>% 
+  left_join(filter(crop_growing, crop=="Maize")) %>% 
+  mutate(
+    non_gr_pct = 1 - growing_pct,
+    growing_pct = if_else(variable=="ubound2", NA_real_, growing_pct),
+    non_gr_pct = if_else(variable=="ubound1", NA_real_, non_gr_pct),
+    growing_pct = if_else(region %in% no_dna_reg, 1, growing_pct)
+    ) %>% 
+  mutate(pct = if_else(!is.na(growing_pct), growing_pct, non_gr_pct)) %>% 
+  select(-growing_pct, -non_gr_pct)
+
+
+write_csv(ubounds_w4, file = "dynamics_presentation/data/ubounds_w4.csv")
 
 
 
